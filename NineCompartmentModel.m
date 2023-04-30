ede8b8a1c368c4d01af96becb1139383e955180d
qayyumayaan <96453808+qayyumayaan@users.noreply.github.com>
2023-04-30 01:06:08 -0400

    Variable Name Renaming

--- NineCompartmentModel.m
+++ NineCompartmentModel.m
@@ -41,32 +41,32 @@ close all
 
 CO2Pklocs = ietCO2;
 
-Vv = 4000; % ml, venous blood volume, from cardiac output
-V_D = 150; % ml, Anatomical dead space
+VenousBloodVolume = 4000; % ml, venous blood volume, from cardiac output
+anatomicDeadSpaceVol = 150; % ml, Anatomical dead space
 VCO2 = 248; % ml min-1, exhaled volume of CO2
 VO2n = 263; % ml min-1, Pulmonary O2 Uptake. Q * (ConcO2art - ConcO2ven)
 RQ = .9; % Respiratory quotient, taken from paper. VCO2/VO2
 Va = 1300; % ml, Arterial blood volume
 c = .1316; %/mmHg
 
-CO2vn1 = ones([9 1]); % %, Venous partial CO2 content
-CO2an1 = ones([9 1]); % %, Arterial CO2 content
-PkCO2n1 = ones([9 1]); % mmHg, Lung compartment k partial CO2 pressure. Necessary output!
+venousPartialCO2 = ones([9 1]); % %, Venous partial CO2 content
+arterialCO2 = ones([9 1]); % %, Arterial CO2 content
+partialCO2PressurePerCompartment = ones([9 1]); % mmHg, Lung compartment k partial CO2 pressure. Necessary output!
 CO2v = ones([9 1]);
 CO2a = ones([9 1]);
 
-BN = length(CO2Pklocs); % # of breaths
-NCO2 = zeros([9 BN]);
-PETCO2 = ones([1 BN]);
-PkCO2 = zeros([9 BN]); % mmHg, Lung compartment k partial CO2 pressure. Necessary output!
+numBreaths = length(CO2Pklocs); % # of breaths
+NCO2 = zeros([9 numBreaths]);
+PETCO2 = ones([1 numBreaths]);
+lungCompPartialCO2 = zeros([9 numBreaths]); % mmHg, Lung compartment k partial CO2 pressure. Necessary output!
 
-Trespn = 0; % s, Respiratory interval. See line ~148. 
+respiratoryInterval = 0; % s, Respiratory interval. See line ~148. 
 
 defFRCV_Cap = [6.58, 8.64, 10.11, 11.16, 11.90, 12.43, 12.81, 13.08, 13.27]';
 
-V_Tn = 700*[4.58, 6.63, 8.48, 10.13, 11.62, 12.96, 14.17, 15.25, 16.22]'/100; % ml, Tidal volume. was just .5
-FRC = 3000*defFRCV_Cap/100; % mL Functional residual capacity. was just 3
-V_Cap = 200*defFRCV_Cap/100; % ml, Lung capillary blood volume. was just 75
+tidalVol = 700*[4.58, 6.63, 8.48, 10.13, 11.62, 12.96, 14.17, 15.25, 16.22]'/100; % ml, Tidal volume. was just .5
+functionalResidualCapacity = 3000*defFRCV_Cap/100; % mL Functional residual capacity. was just 3
+lungCapillaryBloodVol = 200*defFRCV_Cap/100; % ml, Lung capillary blood volume. was just 75
 
 g_k = ones([9 1]);
 h_k = ones([9 1]);
@@ -97,33 +97,33 @@ w_k(k) = .10055*(1.36708 - exp(-.3393*k));
 SV_adj_ml = SV_adj * 1000;
 SVn = SV_adj_ml.*[.58; 3.21; 5.84; 8.47; 11.10; 13.73; 16.36; 18.99; 21.62]/100;
 
-for L = 1:BN
+for L = 1:numBreaths
 
     PETCO2n1 = etCO2(L); % mmHg, end-tidal partial CO2 pressure
 
-    C = CO2vn1.*SVn; % 2, ml * %
+    C = venousPartialCO2.*SVn; % 2, ml * %
 
-    A = CO2an1.*SVn; % 3, ml * %
+    A = arterialCO2.*SVn; % 3, ml * %
 
-    B = VO2n.*RQ.*(Trespn/60); % 4, ml, est. CO2 produced per breath
+    B = VO2n.*RQ.*(respiratoryInterval/60); % 4, ml, est. CO2 produced per breath
 
-    CO2vn = CO2vn1 + (A + B - C) / Vv ; % 1, %
+    CO2vn = venousPartialCO2 + (A + B - C) / VenousBloodVolume ; % 1, %
 
-    D = f(PkCO2n1) .* g_k .* SVn; % 6
+    D = f(partialCO2PressurePerCompartment) .* g_k .* SVn; % 6
 
-    E = CO2an1.*SVn; % 7, ml * %
+    E = arterialCO2.*SVn; % 7, ml * %
 
-    CO2an = CO2an1 + (D-E)/Va; % 5, %
+    CO2an = arterialCO2 + (D-E)/Va; % 5, %
 
-    F = f(PkCO2n1).* w_k .* V_Cap + c .* PkCO2n1 .* w_k .* FRC + c .* PETCO2n1.* w_k.* V_D; % 8
+    F = f(partialCO2PressurePerCompartment).* w_k .* lungCapillaryBloodVol + c .* partialCO2PressurePerCompartment .* w_k .* functionalResidualCapacity + c .* PETCO2n1.* w_k.* anatomicDeadSpaceVol; % 8
 
-    G = CO2vn1 .* SVn .* g_k; % 9
+    G = venousPartialCO2 .* SVn .* g_k; % 9
 
     a = f(PETCO2n1) ./ (c * PETCO2n1); % 10
 
-    b = (w_k .* FRC + h_k .* V_Tn) ./ (a.*((w_k .* V_Cap + g_k .* SVn) + w_k .* FRC + h_k .* V_Tn)); % 11
+    b = (w_k .* functionalResidualCapacity + h_k .* tidalVol) ./ (a.*((w_k .* lungCapillaryBloodVol + g_k .* SVn) + w_k .* functionalResidualCapacity + h_k .* tidalVol)); % 11
 
-    CO2kn = b.*(F + G) ./ (w_k .* FRC + h_k .* V_Tn); % 12, %
+    CO2kn = b.*(F + G) ./ (w_k .* functionalResidualCapacity + h_k .* tidalVol); % 12, %
 
     PkCO2n = CO2kn.*c; % mmHg
 
@@ -131,7 +131,7 @@ for L = 1:BN
 
     % variables indexed to save per loop
     NCO2(:,L) = CO2kn(:);
-    PkCO2(:,L) = PkCO2n(:);
+    lungCompPartialCO2(:,L) = PkCO2n(:);
     CO2v(:,L) = CO2vn(:);
     CO2a(:,L) = CO2an(:);
     PETCO2(:,L) = PETCO2n(:);
@@ -139,16 +139,16 @@ for L = 1:BN
     fprintf("Breath %d computed. ",L)
 
     %PkCO2n./PkCO2n1 % value details if system is stable. 
-    PkCO2n1 = PkCO2n;
-    CO2vn1 = CO2vn;
-    CO2an1 = CO2an;
+    partialCO2PressurePerCompartment = PkCO2n;
+    venousPartialCO2 = CO2vn;
+    arterialCO2 = CO2an;
 
 
-    Trespn = (CO2Pklocs(1,L)-Trespn)/1000; % s, Respiratory interval
+    respiratoryInterval = (CO2Pklocs(1,L)-respiratoryInterval)/1000; % s, Respiratory interval
 
 end
 disp("Finished.")
-disp(PkCO2);
+disp(lungCompPartialCO2);
 
 
 subplot(1,2,1)
@@ -160,7 +160,7 @@ xlabel('breath')
 legend('Real ETCO2','Estimated ETCO2')
 
 subplot(1,2,2)
-plot(PkCO2')
+plot(lungCompPartialCO2')
 title('Pressure in 9 compartments')
 ylabel('P_{CO_2} (mmHg)')
 xlabel('breath')
